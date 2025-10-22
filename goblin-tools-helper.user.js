7852220543f7709c27928a13586f820f9735a303
bifangknt <1320414964@qq.com>
2025-10-22 14:38:32 +0800

    fix: 修正拖拽功能中的全角句号为半角点号
    修复事件监听器中误用的全角句号，确保JavaScript语法正确执行

--- goblin-tools-helper.user.js
+++ goblin-tools-helper.user.js
@@ -273,12 +273,12 @@
 
     function makeDraggable(element, handle) {
         let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-        handle。onmousedown = function(e) {
-            e。preventDefault(); pos3 = e.clientX; pos4 = e.clientY;
-            document。onmouseup = closeDragElement; document。onmousemove = elementDrag;
+        handle.onmousedown = function(e) {
+            e.preventDefault(); pos3 = e.clientX; pos4 = e.clientY;
+            document.onmouseup = closeDragElement; document.onmousemove = elementDrag;
         };
         function elementDrag(e) {
-            e。preventDefault(); pos1 = pos3 - e.clientX; pos2 = pos4 - e.clientY;
+            e.preventDefault(); pos1 = pos3 - e.clientX; pos2 = pos4 - e.clientY;
             pos3 = e.clientX; pos4 = e.clientY;
             element.style.top = (element.offsetTop - pos2) + "px";
             element.style.left = (element.offsetLeft - pos1) + "px";
